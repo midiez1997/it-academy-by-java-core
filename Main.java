3d0bf6b8dbceb30d5f16d72497b9ad35f9739d9a
Dmitry S <71866608+midiez1997@users.noreply.github.com>
2020-09-30 15:31:09 +0300

    zawork book

---
+++ Main.java
@@ -0,0 +1,12 @@
+package com.company.sedun;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(sum(8, 10));
+    }
+    public static int sum(int x, int y) {
+        int z = (x+y) + (x*y);
+        return z;
+        }// write your code here
+        }
