34a59f2cff14d801fa28ca0be40574ad1ffcbee2
Dmitry S <71866608+midiez1997@users.noreply.github.com>
2020-09-30 15:42:21 +0300

    zadanie 3

--- Main.java
+++ Main.java
@@ -3,22 +3,23 @@ package com.company.sedun;
 public class Main {
 
     public static void main(String[] args) {
-	int s = 3700;
-	int sec;
-	int m;
-	int min, h;
-	int sut;
-	int weeks;
-	int weeks2;
-	int l;
-	sec = s % 60;
-	m = (s-sec) / 60;
-	min = m%60;
-	h = (m-min) / 60;
-    sut = h%24;
-    l = (h-sut) / 24;
-    weeks = l%7;
-    weeks2 = (l-weeks) / 7;
-	System.out.println(h + " часов " + min + " минут " + sec + " секунд " + sut + " суток " + weeks2 + " недель ");// write your code here
+    byte b =0x55;
+    short s = 0x55ff;
+    int i = 1000000;
+    long L = 0xfffffffL;
+    char c = 'a';
+    float f = .25f;
+    double d = .00001234;
+    boolean bool = true;
+    System.out.println(b);
+    System.out.println(s);
+    System.out.println(L);
+    System.out.println(c);
+    System.out.println(f);
+    System.out.println(d);
+    System.out.println(bool);
+
+
+	// write your code here
     }
 }
