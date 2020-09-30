1b97a9c954489f9c5ed5c820b57fdb109be4ec49
Dmitry S <71866608+midiez1997@users.noreply.github.com>
2020-09-30 15:50:21 +0300

    zadanie 5

--- Main.java
+++ Main.java
@@ -3,10 +3,22 @@ package com.company.sedun;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(sum(8, 10));
+	int s = 3700;
+	int sec;
+	int m;
+	int min, h;
+	int sut;
+	int weeks;
+	int weeks2;
+	int l;
+	sec = s % 60;
+	m = (s-sec) / 60;
+	min = m%60;
+	h = (m-min) / 60;
+    sut = h%24;
+    l = (h-sut) / 24;
+    weeks = l%7;
+    weeks2 = (l-weeks) / 7;
+	System.out.println(h + " часов " + min + " минут " + sec + " секунд " + sut + " суток " + weeks2 + " недель ");// write your code here
     }
-    public static int sum(int x, int y) {
-        int z = (x+y) + (x*y);
-        return z;
-        }// write your code here
-        }
+}
