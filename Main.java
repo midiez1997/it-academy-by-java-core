6ddc349493f4343dae18a9b4f424ec736c916491
Dmitry S <71866608+midiez1997@users.noreply.github.com>
2020-09-30 15:49:17 +0300

    zadanie 4

--- Main.java
+++ Main.java
@@ -3,23 +3,10 @@ package com.company.sedun;
 public class Main {
 
     public static void main(String[] args) {
-    byte b =0x55;
-    short s = 0x55ff;
-    int i = 1000000;
-    long L = 0xfffffffL;
-    char c = 'a';
-    float f = .25f;
-    double d = .00001234;
-    boolean bool = true;
-    System.out.println(b);
-    System.out.println(s);
-    System.out.println(L);
-    System.out.println(c);
-    System.out.println(f);
-    System.out.println(d);
-    System.out.println(bool);
-
-
-	// write your code here
+        System.out.println(sum(8, 10));
     }
-}
+    public static int sum(int x, int y) {
+        int z = (x+y) + (x*y);
+        return z;
+        }// write your code here
+        }
